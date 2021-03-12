1f2b11058becf3dc7dbb3cb1530acae465359d27
jondo-kei <mk.foreverlove.music@icloud.com>
2021-03-12 19:55:11 +0000

    MYSQL設定ファイルコミット

--- next_trident_website/settings.py
+++ next_trident_website/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
+from dotenv import (
+    find_dotenv,
+    load_dotenv,
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +81,9 @@ WSGI_APPLICATION = 'next_trident_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+load_dotenv(find_dotenv())
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':dj_database_url.config(conn_max_age=600),
 }
 
 
